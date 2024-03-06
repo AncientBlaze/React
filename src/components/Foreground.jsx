42726b4c93f47ed64188e1ad5f805035f6ac7032
AncientBlaze <saikata62@gmail.com>
2024-03-07 02:53:01 +0530

    added add button

--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,59 +1,43 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-key */
-
-import { useRef } from "react";
+import { CiSquarePlus } from "react-icons/ci";
+import { useRef, useState } from "react";
 import Card from "./Card";
+
+const Carddata = {
+    desc: "This is magic.",
+    filesize: "0.9mb",
+    close: false,
+    tag: {
+        isOpen: true,
+        tagTitle: "Download Now",
+        tagColor: "green",
+    },
+};
 
 function Foreground() {
     const ref = useRef(null);
-    const data = [{
-        desc: "This is magic.",
-        filesize: "0.9mb",
-        close: false,
-        tag: {
-            isOpen: true,
-            tagTitle: "Download Now",
-            tagColor: "green"
-        }
-    },
-    {
-        desc: "This is magic.",
-        filesize: "0.9mb",
-        close: false,
-        tag: {
-            isOpen: true,
-            tagTitle: "Check this out",
-            tagColor: "blue"
-        }
-    },
-    {
-        desc: "This is magic.",
-        filesize: "0.9mb",
-        close: false,
-        tag: {
-            isOpen: true,
-            tagTitle: "Click here",
-            tagColor: "red"
-        }
-    },
-    {
-        desc: "This file is corrupted",
-        filesize: "0.9mb",
-        close: false,
-        tag: {
-            isOpen: true,
-            tagTitle: "Click Me?",
-            tagColor: "amber"
-        }
-    }
-    ];
+    const [data, setData] = useState([Carddata]); // Initialize data state
+
+    const handleAddCard = () => {
+        setData([...data, Carddata]);
+    };
+
     return (
-        <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-5'>
-            {data.map((item, _index) =>
-                (<Card data={item} reference = {ref} />)
-            )}
-        </div>
-    )
+        <>
+            <div ref={ref} className="fixed top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap p-5">
+                {data.map((item, index) => (
+                    <Card key={index} data={item} reference={ref} />
+                ))}
+                <button
+                    className="hover:bg-zinc-600 rounded-full h-20 w-20 flex justify-end items-end absolute bottom-0 right-0"
+                    onClick={handleAddCard}
+                >
+                    <CiSquarePlus className="h-full w-full" color="yellow" />
+                </button>
+            </div>
+        </>
+    );
 }
 
 export default Foreground;
